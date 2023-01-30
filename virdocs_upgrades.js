91cf3fbeb73f4e2a59277aa28bec0ce74a8b1404
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-01-30 10:52:19 -0800

    refactor+doc: js changes
    -renamed tocInterval to updateStyling for future style changes
    
    -renamed updateChapterStyle to updateTocChapterStyle for clarity
    
    -removed unecessary comments
    
    -added doc to updateStyling

--- virdocs_upgrades.js
+++ virdocs_upgrades.js
@@ -1,19 +1,17 @@
-function updateChapterStyle() {
-    // get the table of contents
+function updateTocChapterStyle() {
     var toc = document.getElementsByClassName("toc-list-content")[0];
     var tocItems = toc.getElementsByTagName("li");
     for (var i = 0; i < tocItems.length; i++) {
         var tocItem = tocItems[i];
         if (tocItem.innerText.startsWith("Chapter")) {
-            // add a class to the toc item
             tocItem.classList.add("chapter-section");
         }
     }
 }
 
-// keep checking if the table of contents is loaded
-var tocInterval = setInterval(function() {
+// Regularly check if styled elements are loaded and update them
+var updateStyling = setInterval(function() {
     if (document.getElementsByClassName("toc-list-content")[0]) {
-        updateChapterStyle();
+        updateTocChapterStyle();
     }
 }, 100);
